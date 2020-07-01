55142b3472e1f3d56266931f22af5b6f5abe65e5
Keno Fischer <keno@juliacomputing.com>
2020-07-01 18:23:58 -0400

    Update API

--- dependents/GPUCompiler/src/irgen.jl
+++ dependents/GPUCompiler/src/irgen.jl
@@ -128,7 +128,7 @@ Core.Compiler.OptimizationParams(ni::GPUInterpreter) = ni.opt_params
 Core.Compiler.may_optimize(ni::GPUInterpreter) = true
 Core.Compiler.may_compress(ni::GPUInterpreter) = true
 Core.Compiler.may_discard_trees(ni::GPUInterpreter) = true
-Core.Compiler.mark_dynamic!(ni::GPUInterpreter, sv::InferenceState, msg) = nothing
+Core.Compiler.add_remark!(ni::GPUInterpreter, sv::InferenceState, msg) = nothing
 
 ### world view of the cache
 
